539ecdd7955c498fdf66f97618c572cc3b451cd4
Valeri <valeri125we@gmail.com>
2021-02-04 22:04:18 +0200

    up

--- src/main/java/com/stressthem/app/services/EmailServiceImpl.java
+++ src/main/java/com/stressthem/app/services/EmailServiceImpl.java
@@ -1,46 +1,48 @@
 package com.stressthem.app.services;
 
 import com.stressthem.app.services.interfaces.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import javax.mail.internet.MimeMessage;
-import java.util.Random;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-
-    @Value("${spring.mail.username}")
-    private  final String STRESSER_EMAIL =null;
-
-    private JavaMailSender sender;
-
-    @Autowired
-    public EmailServiceImpl(JavaMailSender sender) {
-        this.sender = sender;
-    }
-
     @Override
     public void sendConfirmationEmail(String to, String title, String description) {
-            SimpleMailMessage msg=new SimpleMailMessage();
-            msg.setFrom(STRESSER_EMAIL);
-            msg.setTo(to);
-            msg.setSubject(title);
-            msg.setText(description);
-            sender.send(msg);
+
     }
 
     @Override
-    public void sendEmail(String senderName, String senderEmail, String message){
-      SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
-      simpleMailMessage.setFrom(STRESSER_EMAIL);
-      simpleMailMessage.setTo(senderEmail);
-      simpleMailMessage.setSubject("Request WTF Support");
-      simpleMailMessage.setText(message);
-      sender.send(simpleMailMessage);
+    public void sendEmail(String name, String email, String message) {
 
     }
+
+//    @Value("${spring.mail.username}")
+//    private  final String STRESSER_EMAIL =null;
+//
+//    private JavaMailSender sender;
+//
+//    @Autowired
+//    public EmailServiceImpl(JavaMailSender sender) {
+//        this.sender = sender;
+//    }
+//
+//    @Override
+//    public void sendConfirmationEmail(String to, String title, String description) {
+//            SimpleMailMessage msg=new SimpleMailMessage();
+//            msg.setFrom(STRESSER_EMAIL);
+//            msg.setTo(to);
+//            msg.setSubject(title);
+//            msg.setText(description);
+//            sender.send(msg);
+//    }
+//
+//    @Override
+//    public void sendEmail(String senderName, String senderEmail, String message){
+//      SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+//      simpleMailMessage.setFrom(STRESSER_EMAIL);
+//      simpleMailMessage.setTo(senderEmail);
+//      simpleMailMessage.setSubject("Request WTF Support");
+//      simpleMailMessage.setText(message);
+//      sender.send(simpleMailMessage);
+//
+//    }
 }
--- src/main/java/com/stressthem/app/web/schedules/impl/BackupSchedulerImpl.java
+++ src/main/java/com/stressthem/app/web/schedules/impl/BackupSchedulerImpl.java
@@ -4,12 +4,8 @@ import com.stressthem.app.web.schedules.BackupScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.mail.Folder;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
 
 @Component
 public class BackupSchedulerImpl implements BackupScheduler {
@@ -21,7 +17,7 @@ public class BackupSchedulerImpl implements BackupScheduler {
     public void databaseBackup() throws IOException {
         String currentPath=System.getProperty("user.dir")+"/local/db-backup";
         Process process = Runtime.getRuntime().exec("cmd /c dir & docker exec -i my_db mysqldump -uroot -p1234 --databases stress_them_db --skip-comments > backup.sql",null,new File(currentPath));
-
+        System.out.println("Backup works");
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 //        String line = "";
 //        while ((line = reader.readLine()) != null) {
